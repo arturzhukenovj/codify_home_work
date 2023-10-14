7cd1bded1b8e4d24069cbb759ed39db960655a99
Artur <arturkulnazarov@gmail.com>
2023-10-14 16:37:21 +0300

    JS HW

---
+++ JS HW ''1''/main.js
@@ -0,0 +1,24 @@
+//!  Задача 1: Основы переменных.
+
+const a = "Arthur"
+const b = 17
+const c = "Bishkek"
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+//!  Задача 2: Типы данных.
+
+const loveNum = 777
+const loveColor = "white"
+
+console.log(typeof loveNum)
+console.log(typeof "")
+
+// !  Задача 3: Строки.
+
+const text = "Hello "
+const text1 ="world"
+
+console.log(text + text1)
